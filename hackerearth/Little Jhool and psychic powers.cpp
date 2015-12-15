d2ad1bd4c33c847f4322e24dca1b6223e115f521
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2015-12-14 21:17:46 -0500

    add Hackerearth problem

---
+++ hackerearth/Little Jhool and psychic powers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define npos string::npos
+
+using namespace std;
+
+int main() {
+    string s;
+    string strzero = "000000", strone = "111111";
+    cin >> s;
+    if (s.find(strzero) != npos || s.find(strone) != npos) {
+        cout << "Sorry, sorry!" << endl;
+    } else {
+
+        cout << "Good luck!" << endl;
+    }
+    return 0;
+}
