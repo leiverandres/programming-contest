3bf116bf675f7dfb9b1c474644e4792ea73ce53c
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2016-02-10 18:20:49 -0500

    training

--- uri/Dividing_X_by_Y.cpp
+++ uri/Dividing_X_by_Y.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie();
-
-  int t;
-  double x, y;
-  cin >> t;
-  while (t--) {
+  int x, y, n;
+  cin >> n;
+  while (n--) {
     cin >> x >> y;
-    if (y == 0) {
-      cout << "divisao impossivel" << endl;
+    if (y != 0) {
+      cout << fixed << setprecision(1) << (double)x/y << endl;
     } else {
-      cout << fixed << setprecision(1) << x/y << endl;
+      cout << "divisao impossivel" << endl;
     }
   }
   return 0;
