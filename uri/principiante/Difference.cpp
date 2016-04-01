3d0c989f77bb677163ebd4e5a43168b5c3597c36
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2016-04-01 12:23:29 -0500

    training

---
+++ uri/principiante/Difference.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi = 3.14159;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << "DIFERENCA = " << (a * b) - (c * d) << endl;
+  return 0;
+}
